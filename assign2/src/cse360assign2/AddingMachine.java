526937938c0f09f8f41f07a035b06eb738ec8615
Jack Summers <jgsummer@asu.edu>
2019-10-07 22:01:55 -0700

    Initial commit.

---
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,53 @@
+package cse360assign2;
+
+/**
+ * @Author: Jack Summers <br>
+ * @StudentID: 1212723250 <br>
+ * @Assignment: 1 <br>
+ * @Description: Adding machine class.
+ */
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * 
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param int value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param int value
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	/**
+	 * 
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * 
+	 */
+	public void clear() {
+	
+	}
+}
